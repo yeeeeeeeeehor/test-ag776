7873f31909ba4247bb6bd03a851e94ef6cb2faaa
Yehor <88080877+yeeeeeeeeehor@users.noreply.github.com>
2023-05-23 20:39:38 +0300

    Update Pro Version.py

--- script/Pro Version.py
+++ script/Pro Version.py
@@ -1,90 +1,83 @@
 import time
-import json
 import logging
-import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-import concurrent.futures
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-num_threads = int(input("Введите количество потоков: "))
-
-urls = []
-with open('url.txt', 'r') as url_file:
-    urls = url_file.readlines()
-
-wallets = []
-with open('wallet.txt', 'r') as wallet_file:
-    wallets = wallet_file.readlines()
 
 # Путь к файлу веб-драйвера Chrome
 driver_path = './chromedriver.exe'
 
-def process_url(url):
+def process_site():
     service = Service(driver_path)
+    options = webdriver.ChromeOptions()
 
-    driver = webdriver.Chrome(service=service)
-
-    driver.get(url.strip())
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get("https://ethereumshanghai.com/")
 
     time.sleep(2)
 
-    while wallets:
+    wallets = []
+    with open('main.txt', 'r') as wallet_file:
+        wallets = wallet_file.readlines()
+
+    while len(wallets) > 0:
         wallet = wallets.pop(0).strip()
-        logging.info(f"Обработка кошелька: {wallet} для ссылки: {url.strip()}")
 
         address_input = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'currency-input--src'))
         )
-
         address_input.clear()
         address_input.send_keys(wallet)
 
         submit_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, '.btn__text:nth-child(3)'))
         )
+        submit_button.click()
+        logging.info(f"Нажата кнопка Submit")
 
         try:
-            time.sleep(2)  # Ожидание 2 секунд после вставки кошелька
+            withdraw_button = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//button[@id='withdraw-records']"))
+            )
+            logging.info("Найдена кнопка Withdraw")
 
-            success = False
-            while not success:
-                submit_button.click()
-                logging.info(f"Нажата кнопка Sumbit: {wallet.strip()}")
+            # Получение данных
+            link_element = driver.find_element(By.XPATH, "//a[@class='copy coin-name font-w--600 color--white ml-1 has-tooltip']")
+            link = link_element.get_attribute('href')
 
-                # Проверка ответа от сайта
-                response = requests.post("https://ethereumshanghai.com/airdrop/submit")
-                if response.status_code == 200:
-                    withdraw_button = driver.find_element(By.XPATH, "//button[@id='withdraw-records']")
-                    logging.info(f"Найдена кнопка Withdraw для кошелька: {wallet}")
-                    success = True
-                elif response.status_code == 429:
-                    logging.warning(f"Ошибка 429 - Превышен лимит запросов. Кошелек: {wallet}")
-                    with open('POST.txt', 'a') as post_file:
-                        post_file.write(f"Ссылка: {url.strip()}, Кошелек: {wallet}\n")
-                    break
-                else:
-                    time.sleep(1)  # Пауза перед следующим нажатием кнопки
+            eths_element = driver.find_element(By.XPATH, "//span[@class='coin-name font-w--600 color--white ml-1']")
+            eths_value = eths_element.text
+
+            count_element = driver.find_elements(By.XPATH, "//span[@class='coin-name font-w--600 color--white ml-1']")[1]
+            count_value = count_element.text
+
+            total_eths_element = driver.find_elements(By.XPATH, "//span[@class='coin-name font-w--600 color--white ml-1']")[2]
+            total_eths_value = total_eths_element.text
+
+            with open('info.txt', 'a') as info_file:
+                info_file.write(f"Кошелек: {wallet}, Ссылка: {link}, ETHS: {eths_value}, Count: {count_value}, Total ETHS: {total_eths_value}\n")
 
         except Exception as e:
-            logging.error(f"Ошибка при нажатии кнопки для кошелька: {wallet}")
+            logging.error("Ошибка при поиске кнопки Withdraw")
             logging.error(str(e))
 
-        with open('log.txt', 'a') as log_file:
-            log_file.write(f"Ссылка: {url.strip()}, Кошелек: {wallet}, Найдена кнопка Withdraw: {str(success)}\n")
+        withdraw_button = driver.find_element(By.XPATH, "//button[@id='withdraw']")
+        withdraw_button.click()
+        logging.info("Нажата кнопка Withdraw")
 
-    driver.quit()
+        time.sleep(3)
 
-num_urls = len(urls)
-num_wallets = len(wallets)
-logging.info(f"Количество ссылок: {num_urls}")
-logging.info(f"Количество кошельков: {num_wallets}")
+        modal_close_button = driver.find_element(By.XPATH, "//div[@id='airdrop-modal']/div/div/div/button/span")
+        modal_close_button.click()
+        logging.info("Нажата кнопка для закрытия модального окна")
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-    executor.map(process_url, urls * (num_wallets // num_threads + 1))
+        time.sleep(1)  # Добавляем задержку перед закрытием веб-драйвера, чтобы страница успела обработать событие закрытия модального окна
 
+        driver.quit()
+
+process_site()
 print("Завершено made by Крипто Підпілля")
